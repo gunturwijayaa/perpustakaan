0f178f2c63b2b9672765bb3aad04262e39d685d6
gunturwijayaa <gunturwijayah@gmail.com>
2023-12-05 02:44:01 +0700

    data masuk ke dalam table

--- js/form-permohonan-dashboard.js
+++ js/form-permohonan-dashboard.js
@@ -1,20 +1,104 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDxhrpBg9U-EynKM1vidldk9cxsqyG_QhU",
-    authDomain: "dummy-kotlin-libe.firebaseapp.com",
-    databaseURL: "https://dummy-kotlin-libe-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "dummy-kotlin-libe",
-    storageBucket: "dummy-kotlin-libe.appspot.com",
-    messagingSenderId: "259175914565",
-    appId: "1:259175914565:web:2362640b70e84040c2d72c"
-  };
-
-  firebase.initializeApp(firebaseConfig);
-
-  // Get a reference to the database
+  // Your web app's Firebase configuration
   
-  var database = firebase.database().ref();
-
-// Mendapatkan referensi tabel HTML
   
+  
+  const firebaseConfig = {
+      apiKey: "AIzaSyDxhrpBg9U-EynKM1vidldk9cxsqyG_QhU",
+      authDomain: "dummy-kotlin-libe.firebaseapp.com",
+      databaseURL: "https://dummy-kotlin-libe-default-rtdb.asia-southeast1.firebasedatabase.app",
+      projectId: "dummy-kotlin-libe",
+      storageBucket: "dummy-kotlin-libe.appspot.com",
+      messagingSenderId: "259175914565",
+      appId: "1:259175914565:web:2362640b70e84040c2d72c"
+    };
 
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database();
+    
+    const permissionsRef = firebase.database().ref('Permissions');
+
+// Mendengarkan perubahan data (realtime)
+permissionsRef.on('value', (snapshot) => {
+    // Mendapatkan data dari snapshot
+    const permissionsData = snapshot.val();
+  
+    // Menemukan elemen tbody
+    const tbody = document.getElementById("data");
+  
+    // Mengosongkan tbody sebelum menambahkan data baru
+    tbody.innerHTML = "";
+  
+    // Iterasi melalui data Permissions
+    for (const key in permissionsData) {
+      const innerObj = permissionsData[key];
+  
+      for (const innerKey in innerObj) {
+        const rowData = innerObj[innerKey];
+  
+        // Mendapatkan UID dari key (misalnya, 'JZTnXzkWLzZnJQ3e242jNKWKspj1')
+        const uid = key;
+  
+        // Mengambil data pengguna dari tabel "Users" berdasarkan UID
+        const userRef = firebase.database().ref('Users/' + uid);
+        userRef.once('value').then((userSnapshot) => {
+          const userData = userSnapshot.val();
+  
+          // Mengambil data buku dari tabel "Books" berdasarkan bookId
+          const bookRef = firebase.database().ref('Books/' + rowData.bookId);
+          bookRef.once('value').then((bookSnapshot) => {
+            const bookData = bookSnapshot.val();
+  
+            // Membuat elemen tr
+            const row = document.createElement("tr");
+  
+            // Kolom pertama
+            const col1 = document.createElement("td");
+  
+            // Menentukan isi kolom pertama
+            col1.innerHTML = userData && userData.nim
+              ? `<i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>${userData.nim}</strong>`
+              : `<i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>${uid}</strong>`;
+  
+            // Menambahkan kolom pertama ke dalam baris
+            row.appendChild(col1);
+  
+            // Kolom kedua
+            const col2 = document.createElement("td");
+            col2.innerHTML = userData && userData.name
+              ? userData.name
+              : 'Nama Tidak Tersedia';
+            row.appendChild(col2);
+  
+            // Kolom ketiga
+            const col3 = document.createElement("td");
+            col3.style.textAlign = "center";
+            col3.innerHTML = bookData && bookData.title
+              ? bookData.title
+              : 'Judul Buku Tidak Tersedia';
+            row.appendChild(col3);
+
+             // Kolom ketiga
+            const col4 = document.createElement("td");
+            col4.style.textAlign = "center";
+            col4.innerHTML = rowData.permission;
+            row.appendChild(col4);
+
+  
+            // Menambahkan baris ke tbody
+            tbody.appendChild(row);
+          }).catch((error) => {
+            console.error("Error getting book data:", error);
+          });
+        }).catch((error) => {
+          console.error("Error getting user data:", error);
+        });
+      }
+    }
+  }, (error) => {
+    console.error("Error getting permissions:", error);
+  });
+
+                  
+
+    
--- js/login.js
+++ js/login.js
@@ -31,63 +31,63 @@ const firebaseConfig = {
   }
 
   // Mendengarkan klik pada tombol Sign In
+
   const buttonSignin = document.getElementById('buttonSignin');
-buttonSignin.addEventListener('click', async (e) => {
-    var email = document.getElementById('email1').value;
-    var password = document.getElementById('password1').value;
-
-    if (email === '' || password === '') {
-        alert('Harap isi semua kolom yang wajib diisi.');
-    } else {
-        try {
-            // Memeriksa apakah akun dengan email yang dimasukkan sudah terdaftar di Realtime Database
-            signInWithEmailAndPassword(auth, email, password)
-                .then(async (userCredential) => {
-                    const user = userCredential.user;
-                    
-                    // Mendapatkan data user dari Realtime Database
-                    const userRef = ref(db, "Users/" + user.uid);
-                    const userSnapshot = await get(userRef);
-                    const userData = userSnapshot.val();
-
-                    // Menggunakan informasi userType dari Realtime Database
-                    const userType = userData ? userData.userType : null;
-
-                    // Memperbarui waktu login terakhir
-                    const dt = new Date();
-                    update(ref(db, "Users/" + user.uid), {
-                        last_login: dt.toISOString(),
-                    });
-
-                    if (userType === "admin") {
-                        alert("Berhasil login!");
-                        localStorage.setItem('isLoggedIn', 'true');
-                        window.location.href = 'form-dashboard.html';
-
-                    } else {
-                        alert("Tipe pengguna tidak valid.");
-                    }
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                
-                    // Menampilkan pesan kesalahan yang lebih rinci di konsol
-                    console.error('Error saat masuk:', errorCode, errorMessage);
-                
-                    // Menampilkan pesan kesalahan kepada pengguna
-                    if (errorCode === 'auth/user-not-found') {
-                        alert('Akun tidak ditemukan. Silakan periksa kembali email Anda.');
-                    } else if (errorCode === 'auth/wrong-password') {
-                        alert('Password salah. Silakan periksa kembali password Anda.');
-                    } else {
-                        alert('Gagal masuk. Silakan cek konsol untuk detail.'+errorMessage+error);
-                    }
-                });                                  
-        } catch (error) {
-            console.error('Error saat mengakses Authentication:', error);
-            alert('Gagal masuk. Harap periksa email dan password Anda.');
-        }
-    }
-});
-
+  buttonSignin.addEventListener('click', async (e) => {
+      var email = document.getElementById('email1').value;
+      var password = document.getElementById('password1').value;
+  
+      if (email === '' || password === '') {
+          alert('Harap isi semua kolom yang wajib diisi.');
+      } else {
+          try {
+              // Memeriksa apakah akun dengan email yang dimasukkan sudah terdaftar di Realtime Database
+              signInWithEmailAndPassword(auth, email, password)
+                  .then(async (userCredential) => {
+                      const user = userCredential.user;
+                      
+                      // Mendapatkan data user dari Realtime Database
+                      const userRef = ref(db, "Users/" + user.uid);
+                      const userSnapshot = await get(userRef);
+                      const userData = userSnapshot.val();
+  
+                      // Menggunakan informasi userType dari Realtime Database
+                      const userType = userData ? userData.userType : null;
+  
+                      // Memperbarui waktu login terakhir
+                      const dt = new Date();
+                      update(ref(db, "Users/" + user.uid), {
+                          last_login: dt.toISOString(),
+                      });
+  
+                      if (userType === "admin") {
+                          alert("Berhasil login!");
+                          localStorage.setItem('isLoggedIn', 'true');
+                          window.location.href = 'form-dashboard.html';
+  
+                      } else {
+                          alert("Tipe pengguna tidak valid.");
+                      }
+                  })
+                  .catch((error) => {
+                      const errorCode = error.code;
+                      const errorMessage = error.message;
+                  
+                      // Menampilkan pesan kesalahan yang lebih rinci di konsol
+                      console.error('Error saat masuk:', errorCode, errorMessage);
+                  
+                      // Menampilkan pesan kesalahan kepada pengguna
+                      if (errorCode === 'auth/user-not-found') {
+                          alert('Akun tidak ditemukan. Silakan periksa kembali email Anda.');
+                      } else if (errorCode === 'auth/wrong-password') {
+                          alert('Password salah. Silakan periksa kembali password Anda.');
+                      } else {
+                          alert('Gagal masuk. Silakan cek konsol untuk detail.'+errorMessage+error);
+                      }
+                  });                                  
+          } catch (error) {
+              console.error('Error saat mengakses Authentication:', error);
+              alert('Gagal masuk. Harap periksa email dan password Anda.');
+          }
+      }
+  });
